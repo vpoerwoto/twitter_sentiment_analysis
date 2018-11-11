e3ede9f842929d7e2998e74a2e4d3444b5835975
Victorio Poerwoto <victorio.poerwoto@allianz.co.id>
2018-11-11 07:14:40 +0700

    Initial commit

---
+++ data/cleancorpus_2014-08-21 11-31-46.sql
@@ -0,0 +1,13 @@
+USE sentigram;
+
+CREATE TABLE `cleancorpus_` (
+  `TEXT_` varchar(150) NOT NULL,
+  `INDO_` tinyint(1) DEFAULT NULL,
+  `JKW_` tinyint(1) DEFAULT NULL,
+  `PRBW_` tinyint(1) DEFAULT NULL,
+  `FLAG_` tinyint(1) DEFAULT NULL,
+  `COUNT_` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TEXT_`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
---
+++ data/label_2014-09-04 07-57-57.sql
@@ -0,0 +1,7 @@
+USE sentigram;
+
+insert into `label_`(`ID_`,`DESC_`) values (0,'UNLABELED');
+insert into `label_`(`ID_`,`DESC_`) values (1,'PRAHA_POS');
+insert into `label_`(`ID_`,`DESC_`) values (2,'PRAHA_NEG');
+insert into `label_`(`ID_`,`DESC_`) values (3,'JKWJK_POS');
+insert into `label_`(`ID_`,`DESC_`) values (4,'JKWJK_NEG');
